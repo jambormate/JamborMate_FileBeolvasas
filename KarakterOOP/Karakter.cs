1c3b1f537d423427d1d940305dabfa7050cd67ed
Petrik Diak <diak@petrik.hu>
2025-02-14 12:09:01 +0100

    1-3 feladat kész

---
+++ KarakterOOP/Karakter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterOOP
+{
+	internal class Karakter
+	{
+		private string nev;
+		private int szint;
+		private int eletero;
+		private int ero;
+
+		public Karakter(string nev, int szint, int eletero, int ero)
+		{
+			this.nev = nev;
+			this.szint = szint;
+			this.eletero = eletero;
+			this.ero = ero;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Szint { get => szint; set => szint = value; }
+		public int Eletero { get => eletero; set => eletero = value; }
+		public int Ero { get => ero; set => ero = value; }
+
+		public override string? ToString()
+		{
+			return $"{nev} - {szint} / {eletero} / {ero}";
+		}
+	}
+}
--- KarakterOOP/Program.cs
+++ KarakterOOP/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace KarakterOOP
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KarakterOOP
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			List<Karakter> karakterek = [];
+
+			Beolvasas("karakterek.txt", karakterek);
+
+            foreach (var item in karakterek)
+            {
+                Console.WriteLine(item);
+            }
+			AtlagosSzint(karakterek);
+			LegmagasabbEletero(karakterek);
+
 		}
+
+		static void Beolvasas(string filenev, List<Karakter> karakterek)
+		{
+			StreamReader sr = new(filenev);
+			sr.ReadLine();
+
+			while (!sr.EndOfStream)
+			{
+				string sor = sr.ReadLine();
+				string[] szavak = sor.Split(';');
+
+				Karakter karakter = new (szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+				karakterek.Add(karakter);
+			}
+		}
+
+		static void LegmagasabbEletero(List<Karakter> karakterek)
+		{
+			Karakter maxhp = karakterek[0];
+			foreach (var item in karakterek)
+			{
+				if (maxhp.Eletero < item.Eletero)
+				{
+					maxhp = item;
+				}
+			}
+            Console.WriteLine($"{maxhp.Nev} - {maxhp.Szint} / {maxhp.Ero}");
+        }
+		
+
+		static void AtlagosSzint(List<Karakter> karakterek)
+		{
+			int ossz = 0;
+            for (int i = 0; i < karakterek.Count; i++)
+            {
+				ossz += karakterek[i].Szint;
+            }
+			int Atlag = ossz/karakterek.Count;
+            Console.WriteLine($"A karakterek átlagszintje: {Atlag}");
+
+        }
 	}
 }
